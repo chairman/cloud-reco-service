adaa8a483fc8e2e4725b1e4c6d16194a9580ad4f
pingping <805029402@qq.com>
2022-10-13 12:38:11 +0800

    nacos mongodb

--- excutor/src/main/java/reco/core/JsonUtils.java
+++ excutor/src/main/java/reco/core/JsonUtils.java
@@ -1,18 +1,18 @@
 package reco.core;
 
-//import com.alibaba.fastjson.JSON;
-//import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonUtils {
-//    public static Map<String,Object> getNotNullFieldToMap(Object javaBean){
-//        JSONObject json = JSON.parseObject(JSON.toJSONString(javaBean));
-//        Map<String,Object> map = new HashMap<>(json.size());
-//        json.forEach((k,v)->{
-//            if(v!=null) map.put(k,v);
-//        });
-//        return map;
-//    }
+    public static Map<String,Object> getNotNullFieldToMap(Object javaBean){
+        JSONObject json = JSON.parseObject(JSON.toJSONString(javaBean));
+        Map<String,Object> map = new HashMap<>(json.size());
+        json.forEach((k,v)->{
+            if(v!=null) map.put(k,v);
+        });
+        return map;
+    }
 }
--- excutor/src/main/java/reco/core/rule/dao/RulesRepositoryImpl.java
+++ excutor/src/main/java/reco/core/rule/dao/RulesRepositoryImpl.java
@@ -1,7 +1,7 @@
 package reco.core.rule.dao;
 
-//import com.alibaba.fastjson.JSON;
-//import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import org.bson.Document;
